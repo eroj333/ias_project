cc1d16f0ad43bf7fd5bf2a1ca0bad00d482ec52b
Enosh Shrestha <eshrestha@alarm.com>
2023-11-05 19:36:40 -0800

    add panoptic segmentation

--- main.py
+++ main.py
@@ -5,18 +5,20 @@ output: panoptic segmentation image, mask images, json file
 """
 
 import argparse
-import os
 import sys
-import time
 import cv2
-import numpy as np
-import torch
-import json
 import logging
-from segmentation import ThresholdSegmentation
+
+import numpy as np
+
+from segmentation import ThresholdSegmentation, CocoPanopticSegmentation
+from utils import load_img, save_img
+import matplotlib.pyplot as plt
+from depth_estimation import MidasDepthEstimator, visualize_3d
+from tracking import HungarianTracker
 
 # format log: <date> <time> <level> <module> <message>
-logging.basicConfig(level=logging.INFO, format='%(asctime)s %(levelname)s %(module)s %(message)s')
+logging.basicConfig(level=logging.DEBUG, format='%(asctime)s %(levelname)s %(module)s %(message)s')
 logger = logging.getLogger(__name__)
 
 
@@ -25,74 +27,12 @@ def parse_arguments():
     Parse input arguments
     """
     args = argparse.ArgumentParser(description='Panoptic Segmentation')
-    args.add_argument('--image', help="path to image", default="test_img.png", type=str)
+    args.add_argument('--image', help="path to image", default="coco_test.jpg", type=str)
     # args.add_argument('--model', help="path to model", default="data/model.pt", type=str)
     args.add_argument('--model_path', help="path to model", default="data/model.pt", type=str)
-    args.add_argument('--method', help="path to model", default="cv2", options=['cv2', 'm2f'], type=str)
+    args.add_argument('--method', help="path to model", default="cocopan", choices=['cv2', 'cocopan'], type=str)
     args.add_argument('--output', help="path to output", default="output", type=str)
     return args.parse_args()
-
-
-def load_model(model_path):
-    """
-    Load model from path
-    """
-    logger.debug(f"Loading model from {model_path}")
-    if not os.path.isfile(model_path):
-        print("Model does not exist")
-        sys.exit(0)
-    model = torch.load(model_path)
-    model.eval()
-    logger.debug("Model loaded")
-    return model
-
-
-def load_img(img_path):
-    """
-    Load image from path
-    """
-    logger.debug(f"Loading image from {img_path}")
-    if not os.path.isfile(img_path):
-        print("Image does not exist")
-        sys.exit(0)
-    img = cv2.imread(img_path)
-    logger.debug("Image loaded")
-    return img
-
-
-def save_img(img, mask, json_dict, original_img_path, path):
-    """
-    Save segmentation details
-    :param img: segmented image
-    :param mask: list of masks
-    :param json_dict: dict containing instance details of each mask
-    :param original_img_path: original image path
-    :param path: save destination
-    """
-    logger.info("Saving segmentation details")
-
-
-    # save segmented image as dest_path + orginal_image_name/segmented.png
-    logger.debug("Saving segmented image")
-    img_name = os.path.basename(original_img_path).split(".")[0]
-    img_path = os.path.join(path, img_name, "segmented.png")
-    os.makedirs(os.path.dirname(img_path), exist_ok=True)
-    cv2.imwrite(img_path, img)
-
-    # save mask images as dest_path + orginal_image_name/mask + mask_id + .png
-    logger.debug("Saving mask images")
-    for i in range(len(mask)):
-        mask_path = os.path.join(path, img_name, "mask", str(i) + ".png")
-        os.makedirs(os.path.dirname(mask_path), exist_ok=True)
-        cv2.imwrite(mask_path, mask[i])
-
-    # save json as dest_path + orginal_image_name/segments.json
-    logger.debug("Saving json")
-    json_path = os.path.join(path, img_name, "segments.json")
-    os.makedirs(os.path.dirname(json_path), exist_ok=True)
-    with open(json_path, 'w') as f:
-        json.dump(str(json_dict), f)
-    logger.info("Saved segmentation details")
 
 
 def resolve_model(args):
@@ -103,6 +43,8 @@ def resolve_model(args):
     """
     if args.method == "cv2":
         return ThresholdSegmentation()
+    elif args.method == "cocopan":
+        return CocoPanopticSegmentation()
     else:
         return None
 
@@ -119,6 +61,36 @@ def visualize(img, masks):
     cv2.imshow("", segments)
 
 
+def bird_view(masks, depth):
+    # Create bird's eye view given instance masks and depth map
+    colors = [(255, 255, 0), (0, 255, 255), (255, 0, 255), (0, 0, 255), (0, 255, 0), (255, 0, 0)]
+    bboxes = []
+    canvas = np.zeros((depth.shape[1], int(np.max(depth)) + 1, 3))
+    logger.debug(f"Created canvas of shape {canvas.shape}")
+    for i, mask in enumerate(masks):
+        indices = np.where(mask == 255)
+        indices = np.array(indices).T
+        print(indices.shape, depth.shape)
+        d = np.array(depth[indices[:, 0], indices[:, 1]], dtype=np.uint8)
+        d = np.expand_dims(d, axis=1)
+        coords = np.hstack((indices, d))
+
+        # project coords to bird's eye view
+        coords = coords[:, 1:]
+        bbox = [
+            np.min(coords[:, 0]),
+            np.min(coords[:, 1]),
+            np.max(coords[:, 0]),
+            np.max(coords[:, 1])
+        ]
+        bboxes.append(bbox)
+        canvas[coords] = colors[i % len(colors)]
+
+    plt.imshow(canvas)
+    plt.show()
+    return bboxes
+
+
 def main(args):
     img = load_img(args.image)
     segmentation_model = resolve_model(args)
@@ -128,8 +100,24 @@ def main(args):
         sys.exit(0)
 
     segmented_img, masks, json = segmentation_model.segment(img)
-    save_img(segmented_img, masks, json, args.image, args.output)
-    visualize(img, masks)
+
+    # visualize(img, masks)
+    depth_estimator = MidasDepthEstimator()
+    depth = depth_estimator.estimate(img)
+
+    logger.debug(f"Depth map shape: {depth.shape}, segmented image shape: {segmented_img.shape}")
+    save_img(segmented_img, masks, json, depth, args.image, args.output)
+    # bboxes = bird_view(masks, depth)
+    # visualize_3d(img, depth)
+    tracker = HungarianTracker()
+
+    for i in range(10):
+        tracked_img = tracker.track(masks, img)
+        # convert to RGB
+        tracked_img = cv2.cvtColor(tracked_img, cv2.COLOR_BGR2RGB)
+        #visualize with plt
+        plt.imshow(tracked_img)
+        plt.show()
 
 
 if __name__ == '__main__':
--- segmentation.py
+++ segmentation.py
@@ -1,5 +1,19 @@
+import logging
+import os
+
 import cv2
 import numpy as np
+import detectron2
+from detectron2.evaluation import COCOEvaluator
+from detectron2.utils.logger import setup_logger
+
+from utils import save_img
+
+setup_logger()
+from detectron2 import model_zoo
+from detectron2.engine import DefaultPredictor, DefaultTrainer
+from detectron2.config import get_cfg
+from detectron2.data.datasets import register_coco_instances, register_coco_panoptic, register_coco_panoptic_separated
 
 
 def get_bbox(mask):
@@ -26,7 +40,9 @@ def get_area(mask):
 
 
 def get_segmented_img(img, masks):
-    colors = [(255, 255, 0), (0, 255, 255), (255, 0, 255)]
+    # colors = [(255, 255, 0), (0, 255, 255), (255, 0, 255)]
+    # set of non repeating colors
+    colors = [(255, 255, 0), (0, 255, 255), (255, 0, 255), (0, 0, 255), (0, 255, 0), (255, 0, 0)]
     segmented_img = np.zeros(img.shape)
     for i in range(len(masks)):
         mask = masks[i]
@@ -99,12 +115,92 @@ class ThresholdSegmentation():
         return instances
 
 
-class Mask2FormerSegmentation():
-    def __init__(self):
-        pass
+class CocoPanopticSegmentation:
+    def __init__(self, model_save_path=None):
+        self.cfg = get_cfg()
+        self.cfg.merge_from_file(model_zoo.get_config_file("COCO-PanopticSegmentation/panoptic_fpn_R_101_3x.yaml"))
+        self.cfg.MODEL.WEIGHTS = model_zoo.get_checkpoint_url("COCO-PanopticSegmentation/panoptic_fpn_R_101_3x.yaml")
+        self.cfg.OUTPUT_DIR = os.path.join("output",
+                                           self.__class__.__name__) if model_save_path is None else model_save_path
+        self.cfg.MODEL.ROI_HEADS.SCORE_THRESH_TEST = 0.5
+        self.cfg.MODEL.PANOPTIC_FPN.COMBINE.INSTANCES_CONFIDENCE_THRESH = 0.5
+        self.predictor = DefaultPredictor(self.cfg)
+        self.logger = logging.getLogger(self.__class__.__name__)
+
+    def train(self, coco_dataset_train_path, coco_dataset_val_path,
+              train_json_path, val_json_path, coco_train_annotations_path, coco_train_segment_path):
+        """
+        Train the model
+        """
+        train_datset = "panoptic_custom_seg_train"
+        val_dataset = "coco_seg_val"
+        metadata = {
+            "thing_classes": ['object', 'nature', 'sky', 'human', 'vehicle'],
+            "thing_dataset_id_to_contiguous_id": {0: 0, 1: 1, 2: 2, 3: 3, 4: 4, 5: 5, 6: 6, 7: 7},
+            "stuff_dataset_id_to_contiguous_id": {0: 0, 1: 1, 2: 2, 3: 3, 4: 4, 5: 5, 6: 6, 7: 7}
+        }
+        register_coco_panoptic_separated(train_datset, panoptic_json=train_json_path,
+                                         panoptic_root=coco_train_annotations_path,
+                                         image_root=coco_dataset_train_path,
+                                         metadata={}, sem_seg_root=coco_train_segment_path,
+                                         instances_json=train_json_path)
+        # register_coco_instances(val_dataset, {}, val_json_path, coco_dataset_val_path)
+        self.cfg.DATASETS.TRAIN = (train_datset,)
+        # self.cfg.DATASETS.TEST = (val_dataset,)
+        self.cfg.DATALOADER.NUM_WORKERS = 2
+        self.cfg.MODEL.WEIGHTS = model_zoo.get_checkpoint_url("COCO-PanopticSegmentation/panoptic_fpn_R_101_3x.yaml")
+        self.cfg.SOLVER.IMS_PER_BATCH = 2
+        self.cfg.SOLVER.BASE_LR = 0.00025
+        self.cfg.SOLVER.MAX_ITER = 5000
+        self.cfg.MODEL.ROI_HEADS.BATCH_SIZE_PER_IMAGE = 128
+        self.cfg.MODEL.ROI_HEADS.NUM_CLASSES = 3
+        os.makedirs(self.cfg.OUTPUT_DIR, exist_ok=True)
+        trainer = DefaultTrainer(self.cfg)
+        trainer.resume_or_load(resume=False)
+        trainer.train()
+
+        # evaluator = COCOEvaluator(val_dataset, output_dir=self.cfg.OUTPUT_DIR)
+        # trainer.test(self.cfg, trainer.model, evaluators=[evaluator])
 
     def segment(self, img):
         """
+        Segments objects from the image
         :param img: image to segment
         :return: segmented image, mask of each object, json containing instance details of each object
         """
+        panoptic_seg, segments_info = self.predictor(img)["panoptic_seg"]
+        panoptic_seg = panoptic_seg.cpu().numpy()
+
+        instances = []
+        for i in range(len(segments_info)):
+            mask = np.zeros(panoptic_seg.shape, dtype="uint8")
+            mask[panoptic_seg == segments_info[i]["id"]] = 255
+            instances.append(mask)
+
+        json = {}
+        for i in range(len(instances)):
+            mask = instances[i]
+            instance_id = i + 1
+            json[instance_id] = {}
+            # json[instance_id]["mask"] = mask.tolist()
+            json[instance_id]["bbox"] = get_bbox(mask)
+            json[instance_id]["area"] = get_area(mask)
+
+        segmented_img = get_segmented_img(img, instances)
+        return segmented_img, instances, json
+
+
+if __name__ == '__main__':
+    dlseg = CocoPanopticSegmentation()
+    img = cv2.imread("coco_test.jpg")
+    segmented_img, instances, json = dlseg.segment(img)
+    # save_img(segmented_img, instances, json, "coco_test.jpg", "output")
+
+    coco_path = "C:\\Users\\eshrestha\\Documents\\kitti\\semantics\\coco\\training\\annotations\\panoptic_kitti_training_2015.json"
+    coco_dataset_path = "C:\\Users\\eshrestha\\Documents\\kitti\\semantics\\coco\\training\\panoptic_kitti_training_2015"
+    coco_train_annotations_path = "C:\\Users\\eshrestha\\Documents\\kitti\\semantics\\coco\\training\\annotations\\panoptic_kitti_training_2015"
+    coco_seg_path = "C:\\Users\\eshrestha\\Documents\\kitti\\semantics\\coco\\training\\segments"
+
+    dlseg.train(train_json_path=coco_path, val_json_path=coco_path, coco_dataset_train_path=coco_dataset_path,
+                coco_train_annotations_path=coco_train_annotations_path,
+                coco_dataset_val_path=coco_dataset_path, coco_train_segment_path=coco_seg_path)
